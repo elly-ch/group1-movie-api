09f87b217c4861c4f9bbd2fc373b594f07e96706
georgie-alx <georgie-alx@gmail.com>
2023-12-05 22:57:18 +0800

    updated dataloader

---
+++ src/main/java/sg/edu/ntu/movie_api/repositories/UserMovieRepository.java
@@ -0,0 +1,9 @@
+package sg.edu.ntu.movie_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sg.edu.ntu.movie_api.entities.UserMovie;
+
+public interface UserMovieRepository extends JpaRepository<UserMovie, Long> {
+
+}
--- src/main/java/sg/edu/ntu/movie_api/serviceImpls/DataLoader.java
+++ src/main/java/sg/edu/ntu/movie_api/serviceImpls/DataLoader.java
@@ -4,28 +4,24 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Component;
 
-import sg.edu.ntu.movie_api.entities.Customer;
-import sg.edu.ntu.movie_api.repositories.CustomerRepository;
-
 @Component
 public class DataLoader {
-    private CustomerRepository customerRepository;
+    private UserMovieRepository userMovieRepository;
 
     // @Autowired
-    public DataLoader(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
+    public DataLoader(UserMovieRepository userMovieRepository) {
+        this.userMovieRepository = userMovieRepository;
     }
 
     @PostConstruct
     public void loadData() {
         // clear the database first
-        customerRepository.deleteAll();
+        userMovieRepository.deleteAll();
 
         // load data here
-        // [Activity 2 - validation]
-        customerRepository.save(new Customer("Bruce", "Banner", "12345678", 1972));
-        customerRepository.save(new Customer("Peter","Parker", "12345677", 2005));
-        customerRepository.save(new Customer("Stephen", "Strange", "12345678", 1976));
-        // customerRepository.save(new Customer("Steve", "Rogers", "12345678", 1940));
+        userMovieRepository.save(new UserMovie(1, 1));
+        userMovieRepository.save(new UserMovie(1, 2));
+        userMovieRepository.save(new UserMovie(2, 3));
+        userMovieRepository.save(new UserMovie(2, 1));
     }
 }
