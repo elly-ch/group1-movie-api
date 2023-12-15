06ab253fe161773c700cfc32c862a477bb632cf9
georgie-alx <georgie-alx@gmail.com>
2023-12-15 23:04:07 +0800

    unit testing for usermovies in controllers

--- src/test/java/sg/edu/ntu/movie_api/controllers/UserControllerTest.java
+++ src/test/java/sg/edu/ntu/movie_api/controllers/UserControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
-    
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -34,12 +34,12 @@ public class UserControllerTest {
 
         // Step 2: Perform the request, get the respinse and assert
         mockMvc.perform(request)
-            // Assert that the status code is 200
-            .andExpect(status().isOk())
-            // Assert that the content type is JSON
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            // Assert that the userid returned is 1
-            .andExpect(jsonPath("$.userid").value(1));
+                // Assert that the status code is 200
+                .andExpect(status().isOk())
+                // Assert that the content type is JSON
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // Assert that the userid returned is 1
+                .andExpect(jsonPath("$.userid").value(1));
     }
 
     @DisplayName("Get all users")
@@ -52,42 +52,80 @@ public class UserControllerTest {
         mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                // one thing to note, for Integration test, is end-to-end, so it actually writes into the database
-                // the value here 3 reflects the actual results return from the database, so you may have to modify your DataLoader.java to make sure it has 3 records inside your database.
+                // one thing to note, for Integration test, is end-to-end, so it actually writes
+                // into the database
+                // the value here 3 reflects the actual results return from the database, so you
+                // may have to modify your DataLoader.java to make sure it has 3 records inside
+                // your database.
                 .andExpect(jsonPath("$.size()").value(3));
+    }
+
+    @DisplayName("Get usermovies")
+    @Test
+    public void getAllUserMoviesTest() throws Exception {
+        // Step 1: Build a GET request to /users/1
+        RequestBuilder request = MockMvcRequestBuilders.get("/users/1/movies");
+
+        // Step 2: Perform the request, get the respinse and assert
+        mockMvc.perform(request)
+                // Assert that the status code is 200
+                .andExpect(status().isOk())
+                // Assert that the content type is JSON
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.size()").value(2));
+    }
+
+    @DisplayName("Get usermovie by userid and movieid")
+    @Test
+    public void getUserMovieTest() throws Exception {
+        // Step 1: Build a GET request to /users/1
+        RequestBuilder request = MockMvcRequestBuilders.get("/users/1/movies/1");
+
+        // Step 2: Perform the request, get the respinse and assert
+        mockMvc.perform(request)
+                // Assert that the status code is 200
+                .andExpect(status().isOk())
+                // Assert that the content type is JSON
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.userid").value(1))
+                .andExpect(jsonPath("$.movieid").value(1));
     }
 
     // @Test
     // public void validCustomerCreationTest() throws Exception {
-    //     Customer customer = Customer.builder().firstName("Clint").lastName("Barton").email("clint@avengers.com")
-    //     .contactNo("12345678").jobTitle("Special Agent").yearOfBirth(1975).build();
+    // Customer customer =
+    // Customer.builder().firstName("Clint").lastName("Barton").email("clint@avengers.com")
+    // .contactNo("12345678").jobTitle("Special Agent").yearOfBirth(1975).build();
 
-    //     String newCustomerAsJSON = objectMapper.writeValueAsString(customer);
+    // String newCustomerAsJSON = objectMapper.writeValueAsString(customer);
 
-    //     RequestBuilder request = MockMvcRequestBuilders.post("/customers").contentType(MediaType.APPLICATION_JSON)
-    //     .content(newCustomerAsJSON);
+    // RequestBuilder request =
+    // MockMvcRequestBuilders.post("/customers").contentType(MediaType.APPLICATION_JSON)
+    // .content(newCustomerAsJSON);
 
-    //     mockMvc.perform(request)
-    //         .andExpect(status().isCreated())
-    //         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-    //         .andExpect(jsonPath("$.id").value(4))
-    //         .andExpect(jsonPath("$.firstName").value("Clint"))
-    //         .andExpect(jsonPath("$.lastName").value("Barton"));
+    // mockMvc.perform(request)
+    // .andExpect(status().isCreated())
+    // .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+    // .andExpect(jsonPath("$.id").value(4))
+    // .andExpect(jsonPath("$.firstName").value("Clint"))
+    // .andExpect(jsonPath("$.lastName").value("Barton"));
     // }
 
     // @Test
     // public void invalidCustomerCreationTest() throws Exception {
 
-    //     Customer invalidCustomer = new Customer(3L, "  ", "  ", "bruce@a.com", "12345678", "Manager", 1990, null);
+    // Customer invalidCustomer = new Customer(3L, " ", " ", "bruce@a.com",
+    // "12345678", "Manager", 1990, null);
 
-    //     String invalidCustomerAsJSON = objectMapper.writeValueAsString(invalidCustomer);
+    // String invalidCustomerAsJSON =
+    // objectMapper.writeValueAsString(invalidCustomer);
 
-    //     RequestBuilder request = MockMvcRequestBuilders.post("/customers")
-    //         .contentType(MediaType.APPLICATION_JSON)
-    //         .content(invalidCustomerAsJSON);
+    // RequestBuilder request = MockMvcRequestBuilders.post("/customers")
+    // .contentType(MediaType.APPLICATION_JSON)
+    // .content(invalidCustomerAsJSON);
 
-    //     mockMvc.perform(request)
-    //         .andExpect(status().isBadRequest())
-    //         .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    // mockMvc.perform(request)
+    // .andExpect(status().isBadRequest())
+    // .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     // }
 }
